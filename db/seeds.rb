588f0edb73d86dffcdd9c3a4e7c7279e078f10b3
MASEMIYABI <miyabi.kanjani@ezweb.ne.jp>
2021-10-07 17:31:09 +0000

    add

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+User.create!(
+  email: 'momoki@gmail.com',
+  password: 'customer1',
+  name: '桃木',
+)
