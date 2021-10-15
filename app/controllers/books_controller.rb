7b029fd1d40c06d784b392cf03a4821ffb0bee5a
MASEMIYABI <miyabi.kanjani@ezweb.ne.jp>
2021-10-15 19:45:52 +0000

    add

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,21 @@
 class BooksController < ApplicationController
     def index
+        @book = Book.new
         @user = current_user
     end
 
+    def create
+        @book = Book.new(book_params)
+        @book.save
+        redirect_to book_path(@book)
+    end
+
     def show
+        @book = Book.find(params[:id])
     end
 
     def new
+        @book = Book.new
     end
 
     def edit
@@ -14,5 +23,10 @@ class BooksController < ApplicationController
 
     def update
     end
+  private
+
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
 
 end
